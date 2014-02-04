717af03677c6284ab1131261472da8ad43715ef6
Alban Peignier <alban@tryphon.eu>
2014-02-04 11:15:49 +0100

    Use a new telnet instance and close connection after each command

--- lib/qemu/monitor.rb
+++ lib/qemu/monitor.rb
@@ -6,18 +6,21 @@ module QEMU
     attr_accessor :host, :port, :timeout
 
     def initialize(attributes = {})
-      attributes = { :host => "localhost", :timeout => 5 }.merge(attributes)
+      attributes = { :host => "localhost", :timeout => 30 }.merge(attributes)
       attributes.each { |k,v| send "#{k}=", v }
     end
 
     def telnet
-      @telnet ||= Net::Telnet::new "Host" => host, "Port" => port, "Timeout" => timeout, "Prompt" => /\(qemu\) /
+      Net::Telnet::new "Host" => host, "Port" => port, "Timeout" => timeout, "Prompt" => /\(qemu\) /
     end
 
     def command(command)
-      telnet.cmd command
+      telnet.tap do |telnet|
+        telnet.cmd command
+        telnet.close
+      end
     end
-   
+
     def reset
       command "system_reset"
     end
