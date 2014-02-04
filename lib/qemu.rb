4812f042a17b07f9f7ee49c8497348234a3b53da
Alban Peignier <alban@tryphon.eu>
2014-02-04 11:56:13 +0100

    Use NullLogger by default

--- lib/qemu.rb
+++ lib/qemu.rb
@@ -1,22 +1,13 @@
-require "logger"
 
 module QEMU
   def self.sh!(command)
     logger.debug "Execute '#{command}'"
     system command or raise "Command failed: '#{command}'"
   end
-
-  @@logger = Logger.new("log/qemu.log")
-  def self.logger
-    @@logger
-  end
-  def self.logger=(logger)
-    @@logger = logger
-  end
-
 end
 
 require 'qemu/version'
+require 'qemu/logger'
 
 require 'qemu/image'
 require 'qemu/daemon'
