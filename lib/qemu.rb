b5bdc72f24ce30ce5df241798278b2fecf377176
Alban Peignier <alban@tryphon.eu>
2014-02-04 11:12:20 +0100

    Add QEMU.sh! to run safely command. Add QEMU.logger

--- lib/qemu.rb
+++ lib/qemu.rb
@@ -1,4 +1,18 @@
+require "logger"
+
 module QEMU
+  def self.sh!(command)
+    logger.debug "Execute '#{command}'"
+    system command or raise "Command failed: '#{command}'"
+  end
+
+  @@logger = Logger.new("log/qemu.log")
+  def self.logger
+    @@logger
+  end
+  def self.logger=(logger)
+    @@logger = logger
+  end
 
 end
 
